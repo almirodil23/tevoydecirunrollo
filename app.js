7613a7bd7f9b68c34ca66a6279335e1ea58cf793
Alvaro <almirodil@gmail.com>
2023-11-22 04:39:47 +0100

    inicial

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+
+app.use(express.json());
+
+app.listen(port,() =>{
+    console.log(`Servidor escuchando desde http://localhost:${port}`)})
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+    });
+    
+app.get('/bienvenida', (req, res) => {
+     res.sendFile(path.join(__dirname, 'public', 'welcome.html'));
+    });
+    
